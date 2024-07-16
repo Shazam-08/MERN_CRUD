67ce240abbcfc1266af3b5bb9e3fe687809d9157
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2024-07-16 12:31:45 +0530

    basic forms added

--- client/src/Components/UpdateUsers.jsx
+++ client/src/Components/UpdateUsers.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function UpdateUsers() {
-  return <div>UpdateUsers</div>;
+  return (
+    <div>
+      <div>
+        <form>
+          <div>
+            <h1>Update User</h1>
+          </div>
+          <div>
+            <label>Name:</label>
+            <input type="text" placeholder="Enter Your Name" />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" placeholder="Enter Your Email" />
+          </div>
+          <div>
+            <label>Age:</label>
+            <input type="text" placeholder="Enter Your Age" />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default UpdateUsers;
--- client/src/Components/Users.jsx
+++ client/src/Components/Users.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Users() {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([
+    {
+      Name: "Shubham",
+      Email: "shubham@gmail.com",
+      Age: 20,
+    },
+  ]);
+  return (
+    <div>
+      <div>
+        <button>
+          <Link to="/create">Add+</Link>
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr>
+                <td>{user.Name}</td>
+                <td>{user.Email}</td>
+                <td>{user.Age}</td>
+                <td>
+                  <button>
+                    <Link to="/update">Edit</Link>
+                  </button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Users;
