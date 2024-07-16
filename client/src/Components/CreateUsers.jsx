cb33d8cf20bfd53f1b0890d69b936d1b7f8512e3
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2024-07-16 12:07:36 +0530

    basic components added

---
+++ client/src/Components/CreateUsers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateUsers() {
+  return <div>CreateUsers</div>;
+}
+
+export default CreateUsers;
---
+++ client/src/Components/UpdateUsers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UpdateUsers() {
+  return <div>UpdateUsers</div>;
+}
+
+export default UpdateUsers;
